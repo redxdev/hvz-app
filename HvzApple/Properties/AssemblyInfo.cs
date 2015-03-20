4f4b5cf321459e543479778399420af4f19da7a1
Sam Bloomberg <sam@redxdev.com>
2015-03-20 13:36:15 -0400

    created home page for ios

--- HvzApple/Properties/AssemblyInfo.cs
+++ HvzApple/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b1debf4-877c-4ea4-a8f2-78beb076f63f")]
+[assembly: Guid("191d96bc-6a70-4808-8f23-f9ae30b23f3d")]
 
 // Version information for an assembly consists of the following four values:
 //
