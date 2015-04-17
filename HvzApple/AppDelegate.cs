df079c25427de1bf1b6a044ef48f2c4e79f92298
Sam Bloomberg <sam@redxdev.com>
2015-04-17 15:14:56 -0400

    show error

--- HvzApple/AppDelegate.cs
+++ HvzApple/AppDelegate.cs
@@ -96,5 +96,10 @@ namespace Hvz
                 });
             });
         }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            new UIAlertView("Error registering push notifications", error.LocalizedDescription, null, "OK", null).Show();
+        }
     }
 }
